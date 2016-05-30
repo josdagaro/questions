f92c6935b4a1d0b03888e574dc074090bda95d47
ivajo26 <ivajodimo26@gmail.com>
2016-05-29 20:10:50 -0500

    new Frontend init

--- client/js/app.js
+++ client/js/app.js
@@ -1,18 +1,1 @@
-'use strict';
-
-var app = angular.module ('Questions', ['ngCookies', 'ui.router', 'ngMaterial', 'ngMessages', 'md.data.table']);
-
-app.config (function ($stateProvider, $urlRouterProvider, $locationProvider) {
-  $urlRouterProvider.otherwise ('/questions/user');
-
-  $stateProvider.state ('user', {    
-    url: '/questions/user',
-    templateUrl: '/questions/views/partials/user.html',
-    controller: 'UserCtrl'
-  });
-
-  $locationProvider.html5Mode (true);
-}).run (function ($state) {
-    $state.go ('user');
-    event.preventDefault ();
-});
+var app = angular.module('Questions', ['ngMaterial']);
