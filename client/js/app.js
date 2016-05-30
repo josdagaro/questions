36983ed842f2b1efaf28946c6fb6e3954cfcb430
ivajo26 <ivajodimo26@gmail.com>
2016-05-30 00:05:34 -0500

    Part_a 90%

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,30 @@
 var app = angular.module('Questions', ['ngMaterial']);
+
+app.controller("Part_A", function ($scope) {
+  $scope.civil_state = "";
+  $scope.children_num;
+
+  $scope.housing = "";
+  $scope.housingOptions = 'En arriendo,Propia y la está pagando,Propia totalmente pagada,De un familiar sin pagar arriendo,De un tercero sin pagar arriendo,Otra'.split(',').map(function (option) {
+    return option;
+  });
+
+  $scope.limitations = [];
+  $scope.performans = "";
+  $scope.limitationsOptions = 'Moverse o caminar,Usar sus brazos y manos,Ver a pesar de usar lentes o gafas,Oír aún con aparatos especiales,Hablar,Entender o aprender,Relacionarse con los demás por problemas mentales o emocionales,Otra limitación permanente'.split(',').map(function (option) {
+    return option;
+  });
+  $scope.toggle = function (item, list) {
+    var idx = list.indexOf(item);
+    if (idx > -1) {
+      list.splice(idx, 1);
+    } else if (item == 'No Tengo') {
+      $scope.limitations = [item];
+    } else {
+      list.push(item);
+    }
+  };
+  $scope.exists = function (item, list) {
+    return list.indexOf(item) > -1;
+  };
+});
--- client/js/controllers.js
+++
@@ -1,3 +0,0 @@
-var n = 1;
-asd;
-sdf;
