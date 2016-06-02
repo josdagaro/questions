259574d17f68ad11ec14ca7ae29847c5ff21e425
ivajo26 <ivajodimo26@gmail.com>
2016-06-02 10:49:42 -0500

    Parte a

--- sources/app.js
+++ sources/app.js
@@ -10,20 +10,37 @@ app.controller("Part_A", function($scope){
   $scope.limitations = [];
   $scope.performans = "";
   $scope.limitationsOptions = ('Moverse o caminar,Usar sus brazos y manos,Ver a pesar de usar lentes o gafas,Oír aún con aparatos especiales,Hablar,Entender o aprender,Relacionarse con los demás por problemas mentales o emocionales,Otra limitación permanente').split(',').map(function (option) { return  option });
+
   $scope.toggle = function (item, list) {
-    var idx = list.indexOf(item);
-    if (idx > -1) {
-      list.splice(idx, 1);
-    }
-    else if (item == 'No Tengo') {
+    var not_have = $scope.exists('No Tengo', $scope.limitations)
+    if(!not_have){
+      var idx = list.indexOf(item);
+      if (idx > -1) {
+        list.splice(idx, 1);
+        if ($scope.performans == item) {
+          console.log('debe ponerse blanco');
+          $scope.performans = "";
+        }
+      } else if (item == 'No Tengo') {
         $scope.limitations = [item];
-
-      }else{
-      list.push(item);
+      } else {
+        list.push(item);
       }
-
+    }else if (not_have && item == 'No Tengo') {
+      $scope.limitations = [];
+    }
   };
+
   $scope.exists = function (item, list) {
     return list.indexOf(item) > -1;
+  };
+
+
+  $scope.on_performans = function() {
+    return !$scope.exists('No Tengo', $scope.limitations) && $scope.limitations.length > 0;
+  };
+
+  $scope.enviar = function() {
+    alert('Soy famoso');
   };
 });
