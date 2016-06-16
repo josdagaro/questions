f622a8c18b6327ff665e3191deb0e3de22db31ac
ivajo26 <ivajodimo26@gmail.com>
2016-06-16 00:52:43 -0500

    Graf

--- client/js/controllers/part_a.js
+++ client/js/controllers/part_a.js
@@ -45,16 +45,19 @@ app.controller("Part_A", function($scope, $http){
 app.controller("admin_Part_A", function($scope, $http){
   $scope.labels1 = ["Soltero(a)", "Casado / Union Libre"];
   $scope.labels3=  ['En arriendo,Propia y la está pagando', 'Propia totalmente pagada', 'De un familiar sin pagar arriendo', 'De un tercero sin pagar arriendo', 'Otra'];
+  $scope.labels5 = ['Ninguna','Moverse o caminar', 'Usar sus brazos y manos', 'Ver a pesar de usar lentes o gafas', 'Oír aún con aparatos especiales', 'Hablar', 'Entender o aprender', 'Relacionarse con los demás por problemas mentales o emocionales', 'Otra limitación permanente'];
   $http.post('http://localhost/?action=PartA/loadData').then(function(res) {
     var data = res.data.dataset;
-    var counter = {'1':[0,0], '3':[0,0,0,0,0,0]};
+    var counter = {'1':[0,0], '3':[0,0,0,0,0,0], '5':[0,0,0,0,0,0,0,0,0]};
     for (var j in data) {
       counter[1][data[j][1]-1] += 1;
       counter[3][data[j][3]-1] += 1;
+      counter[5][data[j][5]] += 1;
     }
     console.log(counter[3]);
     $scope.data1 = counter[1];
     $scope.data3 = counter[3];
+    $scope.data5 = counter[5];
   });
 
 });
